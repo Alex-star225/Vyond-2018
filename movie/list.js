c045a7f7b6596d207cb2b9f3e58701f7e469cb23
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-06-17 14:18:00 -0400

    Update list.js

--- movie/list.js
+++ movie/list.js
@@ -1,6 +1,6 @@
 const movie = require('./main');
 module.exports = function (req, res, url) {
-	if (req.method != 'GET' || url.path != '/movieList') return;
+	if (req.method != 'GET' || url.path != '/ajax/getMovie') return;
 	Promise.all(movie.list().map(movie.meta)).then(a => res.end(JSON.stringify(a)));
 	return true;
 }
